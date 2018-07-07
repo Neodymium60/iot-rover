b1e3ff2cca4cef806c827c97a9d3c2cf9eec6bf9
Neodymium60 <Fj180@hotmail.com>
2018-07-07 14:10:27 +1000

    JSON
    implemented JSON for rover control elements

--- mqttJoyStick.py
+++ mqttJoyStick.py
@@ -3,44 +3,69 @@
 import paho.mqtt.client as mqtt
 import time
 import pygame
+import json
+
+rover_config = {
+   "target": "Rover1",
+   "mqtt_broker": "192.168.1.5"
+}
+
+rover_control = {
+"Joystick": {
+    "axis": {
+        "axis0": "0",
+        "axis1": "0",
+        "axis2": "0",
+        "axis3": "0"
+},
+    "button": {
+        "button0": "0",
+        "button1": "0",
+        "button2": "0",
+        "button3": "0",
+        "button4": "0",
+        "button5": "0",
+        "button6": "0",
+        "button7": "0",
+        "button8": "0",
+        "button9": "0",
+        "button10": "0",
+        "button11": "0"
+    }
+  }
+}
 
 # Setup pygame and key states
 global hadEvent
 had_event = True
-mqtt_broker="192.168.1.5"
-
 pygame.init()
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 screen = pygame.display.set_mode([300,300])
-pygame.display.set_caption("mqttJoyStick - Press [ESC] to quit")
-#axisUpDown = 1 			 # Joystick axis to read for up / down position
-#axisUpDownInverted = False		 # Set this to True if up and down appear to be swapped
-#axisLeftRight = 3			 # Joystick axis to read for left / right position
-#axisLeftRightInverted = False		 # Set this to True if left and right appear to be swapped
+pygame.display.set_caption("mqttJoyStick2 - Press [ESC] to quit")
 
 print 'Waiting for joystick... (press CTRL+C to abort)'
 while True:
     try:
-	try:
-	    pygame.joystick.init()
-	    # Attempt to setup the joystick
-	    if pygame.joystick.get_count() < 1:
-		# No joystick attached, toggle the LED
-		pygame.joystick.quit()
-		time.sleep(0.5)
-	    else:
-		# We have a joystick, attempt to initialise it!
-		joystick = pygame.joystick.Joystick(0)
-		break
-	except pygame.error:
-	    pygame.joystick.quit()
-	    time.sleep(0.5)
+        try:
+            pygame.joystick.init()
+            # Attempt to setup the joystick
+            if pygame.joystick.get_count() < 1:
+                # No joystick attached, toggle the LED
+                pygame.joystick.quit()
+                time.sleep(0.5)
+            else:
+                # We have a joystick, attempt to initialise it!
+                joystick = pygame.joystick.Joystick(0)
+                break
+        except pygame.error:
+            pygame.joystick.quit()
+            time.sleep(0.5)
     except KeyboardInterrupt:
-	# CTRL+C exit, give up
-	print '\nUser aborted'
-	sys.exit()
+        # CTRL+C exit, give up
+        print '\nUser aborted'
+        sys.exit()
 print 'Joystick found'
 joystick.init()
 # Get the name from the OS for the controller/joystick
@@ -54,43 +79,40 @@ print("Number of buttons: {}".format(num_buttons) )
 joy_buttons=[0 for i in range(num_buttons)]
 
 #MQTT
+topic_key=rover_config['target']
+print("Connecting to {} via mqtt broker: {}" .format((topic_key), rover_config['mqtt_broker']))
 client = mqtt.Client()
-#client.on_publish = on_publish
-client.connect(mqtt_broker, 8883, keepalive=30)
+client.connect(rover_config['mqtt_broker'], 8883, keepalive=30)
 client.loop_start()
 
+#Main
 try:
     print 'Press CTRL+C to quit'
     running = True
     hadEvent = False
     # Loop indefinitely
     while running:
-	# Get the latest events from the system
-	had_event = False
-	events = pygame.event.get()
-	# Handle each event individually
-	for event in events:
-	    if event.type == pygame.QUIT:
-		# User exit
-		running = False
-	    elif event.type == pygame.JOYBUTTONDOWN:
-		# A button on the joystick just got pushed down
-		    for i in range(num_buttons):
-			joy_buttons[i]= joystick.get_button(i)
-			topic_key = "buttons/"
-			topic_value = str(joy_buttons)
-		    print("Topic: {} {}".format(topic_key,topic_value) )
-		    (rc, mid) = client.publish(topic_key, topic_value, qos=0)
-	    elif event.type == pygame.JOYAXISMOTION:
-		# A joystick axis has been moved1
-		    for i in range(num_axes):
-			joy_axis[i]= joystick.get_axis(i)
-			topic_key = "joystick/"
-			topic_value = str(joy_axis)
-		    print("Topic: {} {}".format(topic_key,topic_value) )
-		    (rc, mid) = client.publish(topic_key, topic_value, qos=0)
+        # Get the latest events from the system
+        had_event = False
+        events = pygame.event.get()
+        # Handle each event individually
+        for event in events:
+            if event.type == pygame.QUIT:
+                # User exit
+                running = False
+            elif event.type == pygame.JOYBUTTONDOWN:
+                # A button on the joystick just got pushed down
+                    for i in range(num_buttons):
+                        rover_control['Joystick']['button']['button'+str(i)] = joystick.get_button(i)
+                        command=json.dumps(rover_control)
+                    print(command)
+                    (rc, mid) = client.publish(topic_key, command, qos=0)
+            elif event.type == pygame.JOYAXISMOTION:
+                # A joystick axis has been moved
+                    for i in range(num_axes):
+                        rover_control['Joystick']['axis']['axis'+str(i)] = joystick.get_axis(i)
+                        command=json.dumps(rover_control)
+                    print(command)
+                    (rc, mid) = client.publish(topic_key, command, qos=0)
 except KeyboardInterrupt:
     print
-
-
-
--- mqttRemoteControl.py
+++ mqttRemoteControl.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import paho.mqtt.client as mqtt
-import PicoBorgRev
 import time
 import sys
 import subprocess
+import json
 import threading
-
 
 #global variables
 global PBR
@@ -16,41 +15,69 @@ global camera
 global running
 global watchdog
 running = True
-mqtt_broker="127.0.0.1"
-topic_sub='joystick/#'
-sensitivity=0.15
-joy_axis=[]
+
+rover_config = {
+   "target": "Rover1",
+   "mqtt_broker": "192.168.1.5",
+   "sensitivity" : "0.15"
+}
+sensitivity=float(rover_config["sensitivity"])
+
+rover_control = {
+"Joystick": {
+    "axis": {
+        "axis0": "0",
+        "axis1": "0",
+        "axis2": "0",
+        "axis3": "0"
+},
+    "button": {
+        "button0": "0",
+        "button1": "0",
+        "button2": "0",
+        "button3": "0",
+        "button4": "0",
+        "button5": "0",
+        "button6": "0",
+        "button7": "0",
+        "button8": "0",
+        "button9": "0",
+        "button10": "0",
+        "button11": "0"
+    }
+  }
+}
 
 # Settings for the joystick
-axis_up_down = 1			  # Joystick axis to read for up / down position
-axis_up_down_inverted = False		   # Set this to True if up and down appear to be swapped
-axis_left_right = 0			  # Joystick axis to read for left / right position
-axis_left_right_inverted = True 	  # Set this to True if left and right appear to be swapped
-axis_spin = 3
+#axis_up_down = 1                          # Joystick axis to read for up / down position
+axis_up_down_inverted = False              # Set this to True if up and down appear to be swapped
+#axis_left_right = 0                       # Joystick axis to read for left / right position
+axis_left_right_inverted = True           # Set this to True if left and right appear to be swapped
+#axis_spin = 3
 axis_spin_inverted = True
 
 # Setup the PicoBorg Reverse
-PBR = PicoBorgRev.PicoBorgRev()
-#PBR.i2cAddress = 0x44			# Uncomment and change the value if you have changed the board address
-PBR.Init()
-if not PBR.foundChip:
-    boards = PicoBorgRev.ScanForPicoBorgReverse()
-    if len(boards) == 0:
-	print 'No PicoBorg Reverse found, check you are attached :)'
-    else:
-	print 'No PicoBorg Reverse at address %02X, but we did find boards:' % (PBR.i2cAddress)
-	for board in boards:
-	    print '    %02X (%d)' % (board, board)
-	print 'If you need to change the I2C address change the setup line so it is correct, e.g.'
-	print 'PBR.i2cAddress = 0x%02X' % (boards[0])
-    sys.exit()
-#PBR.SetEpoIgnore(True) 		# Uncomment to disable EPO latch, needed if you do not have a switch / jumper
-PBR.SetCommsFailsafe(False)		# Disable the communications failsafe
-PBR.ResetEpo()
+#PBR = PicoBorgRev.PicoBorgRev()
+#PBR.i2cAddress = 0x44                  # Uncomment and change the value if you have changed the board address
+#PBR.Init()
+#if not PBR.foundChip:
+#    boards = PicoBorgRev.ScanForPicoBorgReverse()
+#    if len(boards) == 0:
+#        print 'No PicoBorg Reverse found, check you are attached :)'
+#    else:
+#        print 'No PicoBorg Reverse at address %02X, but we did find boards:' % (PBR.i2cAddress)
+#        for board in boards:
+#            print '    %02X (%d)' % (board, board)
+#        print 'If you need to change the I2C address change the setup line so it is correct, e.g.'
+#        print 'PBR.i2cAddress = 0x%02X' % (boards[0])
+#    sys.exit()
+#PBR.SetEpoIgnore(True)                 # Uncomment to disable EPO latch, needed if you do not have a switch / jumper
+#PBR.SetCommsFailsafe(False)             # Disable the communications failsafe
+#PBR.ResetEpo()
 
 # Power settings
-voltage_in = 1.2 * 10			 # Total battery voltage to the PicoBorg Reverse
-voltage_out = 6.0			 # Maximum motor voltage
+voltage_in = 1.2 * 10                    # Total battery voltage to the PicoBorg Reverse
+voltage_out = 6.0                        # Maximum motor voltage
 
 # Setup the power limits
 if voltage_out > voltage_in:
@@ -61,32 +88,31 @@ else:
 # Timeout thread
 class Watchdog(threading.Thread):
     def __init__(self):
-	super(Watchdog, self).__init__()
-	self.event = threading.Event()
-	self.terminated = False
-	self.start()
-	self.timestamp = time.time()
+        super(Watchdog, self).__init__()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
+        self.timestamp = time.time()
 
     def run(self):
-	timed_out = True
-	# This method runs in a separate thread
-	while not self.terminated:
-	    # Wait for a network event to be flagged for up to one second
-	    if timed_out:
-		if self.event.wait(1):
-		    # Connection
-		    print 'Reconnected...'
-		    timed_out = False
-		    self.event.clear()
-	    else:
-		if self.event.wait(1):
-		    self.event.clear()
-		else:
-		    # Timed out
-		    print 'Timed out...'
-		    timed_out = True
-		    PBR.MotorsOff()
-
+        timed_out = True
+        # This method runs in a separate thread
+        while not self.terminated:
+            # Wait for a network event to be flagged for up to one second
+            if timed_out:
+                if self.event.wait(1):
+                    # Connection
+                    print 'Reconnected...'
+                    timed_out = False
+                    self.event.clear()
+            else:
+                if self.event.wait(1):
+                    self.event.clear()
+                else:
+                    # Timed out
+                    print 'Timed out...'
+                    timed_out = True
+                    #PBR.MotorsOff()
 
 #MQTT thread
 # The callback for when the client receives a CONNACK response from the server.
@@ -95,99 +121,83 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(topic_sub)
+    client.subscribe(rover_config['target'])
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    joy_axis = (msg.payload).strip("[']").split(',')
-
+    msg_decode=str(msg.payload.decode("utf-8","ignore"))
+    rover_control=json.loads(msg_decode) #decode json data
+    axis_left_right=rover_control["Joystick"]["axis"]["axis0"]
+    axis_up_down=rover_control["Joystick"]["axis"]["axis1"]
+    #print("Joystick axis2 = ",rover_control["Joystick"]["axis"]["axis2"])
+    axis_spin=rover_control["Joystick"]["axis"]["axis3"]
     #Setup axis references
     if axis_left_right_inverted:
-	left_right = -float(joy_axis[axis_left_right])
+        left_right = -float(axis_left_right)
     else:
-	left_right = float(joy_axis[axis_left_right])
+        left_right = float(axis_left_right)
 
     if axis_spin_inverted:
-	spin = -float(joy_axis[axis_spin])
+        spin = -float(axis_spin)
     else:
-	spin = float(joy_axis[axis_spin])
+        spin = float(axis_spin)
 
     if axis_up_down_inverted:
-	up_down = -float(joy_axis[axis_up_down])
+        up_down = -float(axis_up_down)
     else:
-	up_down = float(joy_axis[axis_up_down])
+        up_down = float(axis_up_down)
 
     #Drive routine
     #Forward
     if up_down < -sensitivity:
-	if left_right < -sensitivity:
-	    print ("Drive right {}" .format(joy_axis[axis_up_down]))
-	    drive_left = -((up_down - left_right) * max_power)
-	    drive_right = (up_down * max_power)
-	    PBR.SetMotor1(drive_left)
-	    PBR.SetMotor2(drive_right)
+        if left_right < -sensitivity:
+            print ("Drive right {}" .format(axis_up_down))
+            drive_left = -((up_down - left_right) * max_power)
+            drive_right = (up_down * max_power)
+            #PBR.SetMotor1(drive_left)
+            #PBR.SetMotor2(drive_right)
 
-	if left_right > sensitivity:
-	    print ("Drive left {}" .format(joy_axis[axis_up_down]))
-	    drive_left = -(up_down * max_power)
-	    drive_right = ((up_down - -(left_right)) * max_power)
-	    PBR.SetMotor1(drive_left)
-	    PBR.SetMotor2(drive_right)
+        if left_right > sensitivity:
+            print ("Drive left {}" .format(axis_up_down))
+            drive_left = -(up_down * max_power)
+            drive_right = ((up_down - -(left_right)) * max_power)
+            #PBR.SetMotor1(drive_left)
+            #PBR.SetMotor2(drive_right)
 
     else:
-	   print ("Drive forward {}" .format(joy_axis[axis_up_down]))
-	   drive_left = -(up_down * max_power)
-	   drive_right = (up_down * max_power)
-	   PBR.SetMotor1(drive_left)
-	   PBR.SetMotor2(drive_right)
+           print ("Drive forward {}" .format(axis_up_down))
+           drive_left = -(up_down * max_power)
+           drive_right = (up_down * max_power)
+           #PBR.SetMotor1(drive_left)
+           #PBR.SetMotor2(drive_right)
 
     #Reverse
     if up_down > sensitivity:
-	print ("Drive backward {}" .format(joy_axis[axis_up_down]))
-	drive_left = -(up_down * max_power)
-	drive_right = (up_down * max_power)
-	PBR.SetMotor1(drive_left)
-	PBR.SetMotor2(drive_right)
-	#if left_right < -sensitivity:
-	    #print ("Drive right {}" .format(joy_axis[axis_up_down]))
-	    #drive_left = -((up_down - left_right) * max_power)
-	    #drive_right = (up_down * max_power)
-	    #PBR.SetMotor1(drive_left)
-	    #PBR.SetMotor2(drive_right)
-
-	#if left_right > sensitivity:
-	    #print ("Drive left {}" .format(joy_axis[axis_up_down]))
-	    #drive_left = -(up_down * max_power)
-	    #drive_right = ((up_down - -(left_right)) * max_power)
-	    #PBR.SetMotor1(drive_left)
-	    #PBR.SetMotor2(drive_right)
-
-    #else:
-	   #print ("Drive backward {}" .format(joy_axis[axis_up_down]))
-	   #drive_left = -(up_down * max_power)
-	   #drive_right = (up_down * max_power)
-	   #PBR.SetMotor1(drive_left)
-	   #PBR.SetMotor2(drive_right)
+        print ("Drive backward {}" .format(axis_up_down))
+        drive_left = -(up_down * max_power)
+        drive_right = (up_down * max_power)
+        #PBR.SetMotor1(drive_left)
+        #PBR.SetMotor2(drive_right)
 
     #Spin routine
     if spin < -sensitivity:
-	print ("Spin right {}" .format(joy_axis[axis_spin]))
-	drive_left = (spin * max_power)
-	drive_right = (spin * max_power)
-	PBR.SetMotor1(drive_left)
-	PBR.SetMotor2(drive_right)
+        print ("Spin right {}" .format(axis_spin))
+        drive_left = (spin * max_power)
+        drive_right = (spin * max_power)
+        #PBR.SetMotor1(drive_left)
+        #PBR.SetMotor2(drive_right)
 
     if spin > sensitivity:
-	print ("Spin left {}" .format(joy_axis[axis_spin]))
-	drive_left = (spin * max_power)
-	drive_right = (spin * max_power)
-	PBR.SetMotor1(drive_left)
-	PBR.SetMotor2(drive_right)
+        print ("Spin left {}" .format(axis_spin))
+        drive_left = (spin * max_power)
+        drive_right = (spin * max_power)
+        #PBR.SetMotor1(drive_left)
+        #PBR.SetMotor2(drive_right)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(mqtt_broker, 8883, 60)
+client.connect(rover_config['mqtt_broker'], 8883, 60)
 
 try:
     print 'Press CTRL+C to terminate'
@@ -203,7 +213,7 @@ try:
     # handles reconnecting.
     # Other loop*() functions are available that give a threaded interface and a
     # manual interface.
-	client.loop_forever()
+        client.loop_forever()
 
 
 except KeyboardInterrupt:
@@ -211,11 +221,10 @@ except KeyboardInterrupt:
     print '\nUser shutdown'
 finally:
     # Turn the motors off under all scenarios
-    PBR.MotorsOff()
+    #PBR.MotorsOff()
     print 'Motors off'
     # Tell each thread to stop, and wait for them to end
     running = False
-    watchdog.terminated = True
-    watchdog.join()
-    PBR.SetLed(True)
-
+    #watchdog.terminated = True
+    #watchdog.join()
+    #PBR.SetLed(True)
